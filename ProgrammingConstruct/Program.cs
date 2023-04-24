1805194b53c5fef168548983f5522c1508e2de19
“codewitherrors” <codewitherrors1@gmail.com>
2023-04-24 11:54:06 +0530

    Add : Compare Two Integer

---
+++ ProgrammingConstruct/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace ProgrammingConstruct
+{
+    public class Program
+    {
+        public static void Main(string[] args) 
+        {
+            ProgrammingConstruct programmingConstruct = new ProgrammingConstruct();
+            programmingConstruct.AcceptInteger();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ProgrammingConstruct/ProgrammingConstruct.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammingConstruct
+{
+    public class ProgrammingConstruct
+    {
+        //If and Else Statement.
+        //1.Write a C# Sharp program to accept two integers and check whether they are equal or not.
+        public void AcceptInteger()
+        {
+            Console.WriteLine("Enter the Value : ");
+            int firstInteger = Convert.ToInt32(Console.ReadLine());
+            int secondInteger = Convert.ToInt32(Console.ReadLine());
+            if (firstInteger == secondInteger)
+                Console.WriteLine("Both are Equal");
+            else
+                Console.WriteLine("Both are Different");
+        }
+    }
+}
